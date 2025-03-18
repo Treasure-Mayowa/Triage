ac43bc2c8e2693e67907aaf2c6570c4a5135fd18
Treasure Mayowa <treasuremayowa@outlook.com>
2025-03-18 19:52:51 +0100

    Fix layout component to properly render children

--- triageflow/src/app/layout.js
+++ triageflow/src/app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Layout children={children}/>
+        <Layout> 
+          {children}
+        </Layout>
       </body>
     </html>
   )
