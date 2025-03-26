747b71bad10366eabc64cda90d5aa996a6114aa6
Treasure Mayowa <treasuremayowa@outlook.com>
2025-03-26 21:22:39 +0100

    Fix dataUpdate function to fetch to emit messages

--- triageflow/src/app/api/socket/server.js
+++ triageflow/src/app/api/socket/server.js
@@ -1,11 +1,10 @@
-
-
 export async function dataUpdate () {
-    try {
-        await fetch('https://triage-pww1.onrender.com')
-        return { success: true };
-      } catch (error) {
-        console.error("dataUpdate error:", error)
-        return { error: "Failed to update data" }
-      }
+  try {
+      await fetch('https://triage-pww1.onrender.com')
+      await fetch('https://triage-pww1.onrender.com/socket-emergency')
+      return { success: true }
+    } catch (error) {
+      console.error("dataUpdate error:", error)
+      return { error: "Failed to update data" }
     }
+}
